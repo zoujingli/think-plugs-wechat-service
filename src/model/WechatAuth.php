9bc0af3782bacfdeddd4dd083c0cb57ed8ab3710
邹景立 <zoujingli@qq.com>
2025-03-01 22:26:05 +0800

    fix: 更新所有模型的字段IDE提示注释

--- src/model/WechatAuth.php
+++ src/model/WechatAuth.php
@@ -22,6 +22,31 @@ use think\admin\Model;
 
 /**
  * Class WechatAuth
+ *
+ * @property int $auth_time 授权时间
+ * @property int $deleted 删除状态(0未删除,1已删除)
+ * @property int $expires_in Token时限
+ * @property int $id
+ * @property int $status 授权状态(0已取消,1已授权)
+ * @property int $total 统计调用次数
+ * @property string $appkey 应用接口KEY
+ * @property string $appuri 应用接口URI
+ * @property string $authorizer_access_token 授权Token
+ * @property string $authorizer_appid 微信APPID
+ * @property string $authorizer_refresh_token 刷新Token
+ * @property string $businessinfo 业务序列内容
+ * @property string $create_at 创建时间
+ * @property string $func_info 公众号集权
+ * @property string $miniprograminfo 小程序序列内容
+ * @property string $qrcode_url 公众号二维码
+ * @property string $service_type 公众号类型
+ * @property string $service_verify 公众号认证
+ * @property string $user_alias 公众号别名
+ * @property string $user_company 公众号公司
+ * @property string $user_headimg 公众号头像
+ * @property string $user_name 众众号原账号
+ * @property string $user_nickname 公众号昵称
+ * @property string $user_signature 公众号描述
  * @package plugin\wechat\service\model
  */
 class WechatAuth extends Model
