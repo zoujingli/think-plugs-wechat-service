a979d0b0bb4c77eaae0bfd7b62c0db601ef29d46
Anyon <zoujingli@qq.com>
2025-07-08 11:04:48 +0800

    fix: 增加微信开放平台测试专用号

--- src/controller/api/Push.php
+++ src/controller/api/Push.php
@@ -47,8 +47,12 @@ class Push extends Controller
      */
     public function notify(string $appid = ''): string
     {
+        $tests = [
+            'wx570bc396a51b8ff8', 'wx9252c5e0bb1836fc', 'wx8e1097c5bc82cde9', 'wx14550af28c71a144', 'wxa35b9c23cfe664eb', // 公众号测试专用号
+            'wxd101a85aa106f53e', 'wxc39235c15087f6f3', 'wx7720d01d4b2a4500', 'wx05d483572dcd5d8b', 'wx5910277cae6fd970', // 小程序测试专用号
+        ];
         $appid = empty($appid) ? input('appid') : $appid;
-        if (in_array($appid, ['wx570bc396a51b8ff8', 'wxd101a85aa106f53e'])) {
+        if (in_array($appid, $tests)) {
             # 全网发布接口测试
             return PublishHandle::instance()->handler($appid);
         } else {
